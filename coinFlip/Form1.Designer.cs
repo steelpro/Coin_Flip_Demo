2ef348e40ae3a3a2f5a4d918908edb32e85d702c
Zachary Betters <zachbet97@outlook.com>
2016-09-21 08:41:23 -0400

    Flips a coin and keeps track of results.

--- coinFlip/Form1.Designer.cs
+++ coinFlip/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.pbHeads = new System.Windows.Forms.PictureBox();
             this.pbTails = new System.Windows.Forms.PictureBox();
             this.btnFlip = new System.Windows.Forms.Button();
+            this.lblHeads = new System.Windows.Forms.Label();
+            this.lblTails = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbHeads)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbTails)).BeginInit();
             this.SuspendLayout();
@@ -41,7 +45,7 @@
             this.pbHeads.BackColor = System.Drawing.SystemColors.Control;
             this.pbHeads.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pbHeads.Image = ((System.Drawing.Image)(resources.GetObject("pbHeads.Image")));
-            this.pbHeads.Location = new System.Drawing.Point(40, 67);
+            this.pbHeads.Location = new System.Drawing.Point(12, 66);
             this.pbHeads.Name = "pbHeads";
             this.pbHeads.Size = new System.Drawing.Size(168, 149);
             this.pbHeads.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -51,7 +55,7 @@
             // pbTails
             // 
             this.pbTails.Image = ((System.Drawing.Image)(resources.GetObject("pbTails.Image")));
-            this.pbTails.Location = new System.Drawing.Point(40, 67);
+            this.pbTails.Location = new System.Drawing.Point(12, 67);
             this.pbTails.Name = "pbTails";
             this.pbTails.Size = new System.Drawing.Size(168, 149);
             this.pbTails.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -61,7 +65,7 @@
             // 
             // btnFlip
             // 
-            this.btnFlip.Location = new System.Drawing.Point(51, 12);
+            this.btnFlip.Location = new System.Drawing.Point(22, 13);
             this.btnFlip.Name = "btnFlip";
             this.btnFlip.Size = new System.Drawing.Size(147, 48);
             this.btnFlip.TabIndex = 2;
@@ -69,19 +73,62 @@
             this.btnFlip.UseVisualStyleBackColor = true;
             this.btnFlip.Click += new System.EventHandler(this.btnFlip_Click);
             // 
+            // lblHeads
+            // 
+            this.lblHeads.AutoSize = true;
+            this.lblHeads.Location = new System.Drawing.Point(203, 67);
+            this.lblHeads.Name = "lblHeads";
+            this.lblHeads.Size = new System.Drawing.Size(77, 13);
+            this.lblHeads.TabIndex = 3;
+            this.lblHeads.Text = "Heads: 0%, #0";
+            // 
+            // lblTails
+            // 
+            this.lblTails.AutoSize = true;
+            this.lblTails.Location = new System.Drawing.Point(203, 90);
+            this.lblTails.Name = "lblTails";
+            this.lblTails.Size = new System.Drawing.Size(68, 13);
+            this.lblTails.TabIndex = 4;
+            this.lblTails.Text = "Tails: 0%, #0";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(203, 114);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(43, 13);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "Total: 0";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(185, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(147, 47);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(248, 228);
+            this.ClientSize = new System.Drawing.Size(344, 228);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblTails);
+            this.Controls.Add(this.lblHeads);
             this.Controls.Add(this.btnFlip);
             this.Controls.Add(this.pbTails);
             this.Controls.Add(this.pbHeads);
             this.Name = "Form1";
             this.Text = "Flipper";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbHeads)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbTails)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -90,6 +137,10 @@
         private System.Windows.Forms.PictureBox pbHeads;
         private System.Windows.Forms.PictureBox pbTails;
         private System.Windows.Forms.Button btnFlip;
+        private System.Windows.Forms.Label lblHeads;
+        private System.Windows.Forms.Label lblTails;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- coinFlip/Form1.cs
+++ coinFlip/Form1.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using System.Windows.Forms;
-
+/* Zachary Betters
+ * CIS 209
+ * Sept 14, 2016
+ * Coin Flipper
+ * This program will simulate a coin toss
+ */
 namespace coinFlip {
     public partial class Form1 : Form {
+
+        //all variables to be used are declared
+        double headsCount, tailsCount, totCount, headsPerc, tailsPerc;
+
+        private void btnExit_Click(object sender, EventArgs e) { Close(); }
+
         public Form1() { InitializeComponent(); }
-        private void btnFlip_Click(object sender, EventArgs e) {
+
+        private void btnFlip_Click(object sender, EventArgs e) {            
             Random rand = new Random();
             int toss = rand.Next(2);
-            if (toss == 0) {
+            if (toss == 0) { //random number used for toss decides side of coin
                 pbHeads.Visible = true;
                 pbTails.Visible = false;
+                headsCount++; //if heads, number of heads is added by 1                 
             }
             else {
                 pbHeads.Visible = false;
                 pbTails.Visible = true;
+                tailsCount++;
             }
+
+            totCount = tailsCount + headsCount; 
+            //percentages are decided by dividing by total 
+            headsPerc = (headsCount / totCount) * 100;
+            tailsPerc = (tailsCount / totCount) * 100;
+            
+            lblHeads.Text = "Heads: " + headsPerc.ToString("F") + "%, #" + headsCount;
+            lblTails.Text = "Tails: " + tailsPerc.ToString("F") + "%, #" + tailsCount;
+            lblTotal.Text = "Total: " + totCount;
         }
+        private void Form1_Load(object sender, EventArgs e) { }
     }
 }
